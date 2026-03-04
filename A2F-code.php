d029dd889cba98d0fe5514d0bdb392320284d65a
luhan-gui <luhan.guiggia@medjed-dev.fr>
2026-03-04 10:58:47 +0100

    ajout Qr img

---
+++ A2F-code.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Alizon - A2F</title>
+</head>
+<body>
+    <main>
+        <img class="logo" src="img/logo_alizon_front.svg" alt="Logo Alizon">
+        <section class="container">
+            <h4> Voici votre clé secrète : xxxxxxxxxxxxxxx </h4>
+            <img src="img/qr.png" alt="QR Code Alizon">
+            <p> Veuillez entrer cette clé dans votre application d'authentification pour générer les codes de vérification à six chiffres nécessaires pour l'authentification à deux facteurs (A2F). Assurez-vous de conserver cette clé en lieu sûr, car elle est essentielle pour configurer votre compte Alizon avec l'application d'authentification. En cas de perte de cette clé, vous pourriez rencontrer des difficultés pour accéder à votre compte Alizon via l'A2F. </p>
+            <p>Lorque vous avez entré la clé dans votre application d'authentification, veuillez rentrer votre code de vérification à six chiffres ci-dessous pour finaliser l'activation de l'authentification à deux facteurs (A2F) de votre compte Alizon.</p>
+        </section>
+    </main>
+    <?php include 'includes/footer.php';?>
+</body>
+</html>
--- A2F-pres.php
+++ A2F-pres.php
@@ -14,7 +14,7 @@
             <p>L'authentification à deux facteurs (A2F) est une mesure de sécurité supplémentaire qui nécessite une deuxième forme d'identification en plus de votre mot de passe en passant par une application d'authentification*. En activant l'A2F, vous renforcez la protection de votre compte contre les accès non autorisés, même si quelqu'un parvient à obtenir votre mot de passe. Souhaitez-vous activer cette fonctionnalité pour améliorer la sécurité de votre compte Alizon ? <br>
             * Une application tel que Google Authenticator ou Authy est nécessaire pour utiliser l'authentification à deux facteurs (A2F). </p>
         </section>
-        <a class="bouton" href="saisie.php">Activer l'A2F</a>
+        <a class="bouton" href="A2F-code.php">Activer l'A2F</a>
         <a class="btnJaune" href="">Retour</a>
     </main>
     <?php include 'includes/footer.php';?>
